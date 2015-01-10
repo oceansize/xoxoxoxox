37269b11e8ee9c66c217443da8c5c3d39db67f4f
Roi Driscoll <roi.driscoll@gmail.com>
2015-01-10 21:59:40 +0000

    Added marker placement validation

--- lib/grid.rb
+++ lib/grid.rb
@@ -3,11 +3,15 @@ class Grid
   attr_accessor :cells
 
   def initialize
-      @cells = Array.new(9)
+    @cells = Array.new(9)
   end
 
   def place_marker position, marker
-    self.cells[position] = marker
+    cell_valid?(position) ? self.cells[position] = marker : "Invalid cell location - Please try again"
+  end
+
+  def cell_valid? position
+    position.between?(0,8) ? true : false
   end
 
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -2,15 +2,19 @@ require 'grid'
 
 describe 'The grid' do
 
+  let(:grid) { Grid.new }
+
   it 'has 9 cells' do
-    grid = Grid.new
     expect(grid.cells.count).to eq 9
   end
 
   it 'can place a marker on the board' do
-    grid = Grid.new
     grid.place_marker(0, :x)
     expect(grid.cells[0]).to eq :x
+  end
+
+  it 'will raise an error if marker goes out of bounds' do
+    expect(grid.place_marker(20, :x)).to eq "Invalid cell location - Please try again"
   end
 
 end
