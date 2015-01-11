89c90857b46944ba9cae2279fcd97dc43e2bee2f
Roi Driscoll <roi.driscoll@gmail.com>
2015-01-11 19:36:02 +0000

    Add winning combinations

--- lib/rules.rb
+++ lib/rules.rb
@@ -1,5 +1,7 @@
 module Rules
 
+  WINNING_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
   def cell_valid?(position)
     position.between?(0,8) ? true : false
   end
--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -10,4 +10,8 @@ describe 'The rules' do
     expect(phantom_grid.cell_valid?(10)).to be false
   end
 
+  it 'has a list of all winning combinations' do
+    expect(Rules::WINNING_COMBINATIONS).to eq [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+  end
+
 end
